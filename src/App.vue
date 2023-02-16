5c07b68207f365a3c9fc4db1bd41043c8d8650aa
paix26875 <shimada2022@gmail.com>
2023-02-17 04:21:25 +0900

    add icons

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!--  <div>-->
-  <!--    <a href="https://vitejs.dev" target="_blank">-->
-  <!--      <img src="/image/vite.svg" class="logo" alt="Vite logo" />-->
-  <!--    </a>-->
-  <!--    <a href="https://vuejs.org/" target="_blank">-->
-  <!--      <img src="/image/vue.svg" class="logo vue" alt="Vue logo" />-->
-  <!--    </a>-->
-  <!--  </div>-->
-  <h1>hoge</h1>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
