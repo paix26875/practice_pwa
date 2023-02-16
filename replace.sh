0da65d7ad10f468a86397b93907967e455e04279
paix26875 <shimada2022@gmail.com>
2023-02-17 03:06:28 +0900

    fix script

--- replace.sh
+++ replace.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sed -i "s/Vite/VITE/g" index.html
+sed -i "s/Vite/VITE/g" dist/index.html
 
 svg=$(ls dist/ | grep svg)
-sed -i "s/\/$svg/.\/$svg/g" index.html
+sed -i "s/\/$svg/.\/$svg/g" dist/index.html
 
 js=$(ls dist/assets | grep js)
-sed -i "s/\/assets\/$js/.\/assets\/$js/g" index.html
+sed -i "s/\/assets\/$js/.\/assets\/$js/g" dist/index.html
 
 css=$(ls dist/assets | grep css)
-sed -i "s/\/assets\/$css/.\/assets\/$css/g" index.html
+sed -i "s/\/assets\/$css/.\/assets\/$css/g" dist/index.html
 
 manifest=$(ls dist/ | grep webmanifest)
-sed -i "s/\/$manifest/.\/$manifest/g" index.html
+sed -i "s/\/$manifest/.\/$manifest/g" dist/index.html
 
 registerSW=$(ls dist/ | grep registerSW)
-sed -i "s/\/$registerSW/.\/$registerSW/g" index.html
+sed -i "s/\/$registerSW/.\/$registerSW/g" dist/index.html
