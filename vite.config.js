595ceaeb30f82dbea85c331b6025b495e7bb05df
paix26875 <shimada2022@gmail.com>
2023-02-16 23:51:52 +0900

    add app manifest config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,24 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ]
