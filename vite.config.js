1e499c2175a013b5348f2a9e2c5d5faad8a2205e
paix26875 <shimada2022@gmail.com>
2023-02-17 04:16:24 +0900

    fix typo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   publicDir: 'public',
-  bash: '/practice_pwa/',
+  base: '/practice_pwa/',
   plugins: [
     vue(),
     VitePWA({
