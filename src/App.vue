822fb28af6b92acce090ac1dd875f348b5ca1355
paix26875 <shimada2022@gmail.com>
2024-11-13 02:04:01 +0900

    chore: 通知のテストを作成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+const authorizeNotification = () => {
+  Notification.requestPermission().then(function (result) {
+    if (result === 'granted') {
+      randomNotification()
+    }
+  })
+}
+function randomNotification() {
+  const options = {
+    body: '通知だよ〜〜〜',
+    icon: 'vite-logo.svg'
+  }
+  const notify = new Notification('通知のテスト', options)
+  setTimeout(randomNotification, 30000)
+}
+</script>
 
 <template>
   <div class="bg-amber-500">Hello, Vue 3! with Vite PWA</div>
+  <button
+    class="bg-amber-600 text-white px-4 py-2 rounded-2xl"
+    @click="authorizeNotification()"
+  >
+    通知を許可する
+  </button>
 </template>
 
 <style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,63 @@ import TailwindCSS from 'tailwindcss'
 import Autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
+
+const pwaBaseConfig = {
+  scope: 'https://paix26875.github.io/practice_pwa/',
+  includeAssets: ['robots.txt'],
+  injectRegister: 'script',
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: true
+  },
+  strategies: 'generateSW',
+  manifest: {
+    name: 'Vite + Vue',
+    short_name: 'VitePWA',
+    description: 'this app is PWA created with Vite + Vue + Vite Plugin PWA',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'image/pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'image/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ],
+    display: 'standalone'
+  },
+  related_applications: [
+    {
+      platform: 'play',
+      url: 'https://play.google.com/store/apps/details?id=cheeaun.hackerweb'
+    }
+  ]
+}
+
+const customSWConfig = {
+  ...pwaBaseConfig, // BaseConfigを上書きする
+  // registerType: 'autoUpdate',
+  // outDir: path.resolve(__dirname, 'public'),
+  // manifest: manifest,
+  manifestFilename: 'manifest.webmanifest', // Change name for app manifest
+  injectRegister: false, // I register SW in app.ts, disable auto registration
+
+  // HERE! For custom service worker
+  // srcDir: path.resolve(__dirname, 'resources/js/'),
+  filename: 'serviceWorker.js',
+  strategies: 'injectManifest',
+
+  workbox: {
+    // globDirectory: path.resolve(__dirname, 'public'),
+    globPatterns: [
+      '{build,images,sounds,icons}/**/*.{js,css,html,ico,png,jpg,mp4,svg}'
+    ]
+  }
+}
 export default defineConfig({
   publicDir: 'public',
   base: 'https://paix26875.github.io/practice_pwa/',
@@ -17,43 +74,5 @@ export default defineConfig({
       plugins: [TailwindCSS, Autoprefixer]
     }
   },
-  plugins: [
-    vue(),
-    VitePWA({
-      scope: 'https://paix26875.github.io/practice_pwa/',
-      includeAssets: ['robots.txt'],
-      injectRegister: 'script',
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      strategies: 'generateSW',
-      manifest: {
-        name: 'Vite + Vue',
-        short_name: 'VitePWA',
-        description:
-          'this app is PWA created with Vite + Vue + Vite Plugin PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'image/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'image/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ],
-        display: 'standalone'
-      },
-      related_applications: [
-        {
-          platform: 'play',
-          url: 'https://play.google.com/store/apps/details?id=cheeaun.hackerweb'
-        }
-      ]
-    })
-  ]
+  plugins: [vue(), VitePWA(pwaBaseConfig)]
 })
